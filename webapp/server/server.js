53260f3c716814d1793c1e8a16199a7943b17f97
jlim-git <joel.lim.92@gmail.com>
2021-11-30 07:35:17 +0900

    Added descriptive comments to routes

--- webapp/server/server.js
+++ webapp/server/server.js
@@ -11,51 +11,99 @@ app.use(cors({
      origin: '*'
  }));
 
+// ********************************************
+//             Test Routes
+// ********************************************
+
 // Route 1 - register as GET 
 app.get('/hello', routes.hello)
 
-// Route 2 - register as GET 
-app.get('/add_user', routes.add_user)
-
-// Route 2 - register as GET 
-app.get('/login', routes.login)
-
-// Route 2 - register as GET 
-app.get('/logout', routes.logout)
-
-// Route 2 - register as GET 
-app.get('/random_city', routes.random_city)
-
-// Route 3 - register as GET 
-app.get('/trips', routes.all_trips)
-
-// Route 5 - register as GET 
-app.get('/cities/', routes.all_cities)
-// app.get('/cities/:state', routes.all_cities)
-
-// Route 4 - register as GET 
-app.get('/quizCities', routes.quizCities)
-
-// Route 6 - register as GET 
-app.get('/new_trip', routes.new_trip)
-
-// Route 10 - register as GET
-app.get('/trip_pois/', routes.trip_pois)
-
-// Route 7 - register as GET 
-app.get('/trip/attractions', routes.trip_attractions)
-
-// Route 8 - register as GET 
-app.get('/trip/restaurants', routes.trip_restaurants)
-
-// Route 9 - register as GET 
-app.get('/trip/trails', routes.trip_trails)
-
-// Route 6 - register as GET 
-app.get('/delete_trip', routes.delete_trip)
-
 // Route 6 - register as GET 
 app.get('/test', routes.test)
+
+
+// ********************************************
+//             User Account Routes
+// ********************************************
+
+// Register new user
+// requires: username and password --> /add_user?username=<username>&password=<password> (no quotations)
+// returns true if successful; false if username already exists
+app.get('/add_user', routes.add_user)
+
+// Log in user
+// requires: username and password --> /login?username=<username>&password=<password> (no quotations)
+// returns true if successful; redirects to localhost:3000/failed otherwise
+app.get('/login', routes.login)
+
+// Log out user
+// requires: username --> /login?username=<username> (no quotations)
+// always returns true
+app.get('/logout', routes.logout)
+
+
+// ********************************************
+//             Home Page Routes
+// ********************************************
+
+// Random city
+// requires: none
+// returns [{city, state, photo}]
+app.get('/random_city', routes.random_city)
+
+// All trips
+// requires: username --> /trips?username=<username> (no quotations)
+// returns [{tripID, tripName, city, state}]
+app.get('/trips', routes.all_trips)
+
+
+// ********************************************
+//             Quiz Routes
+// ********************************************
+
+// All states and cities
+// requires: none
+// returns [{state, city}], ordered by city
+app.get('/cities/', routes.all_cities)
+
+// Top 3 Cities based on quiz results
+// requires population and personalities --> /quizCities?population=1&p0=0&p1=1&...&p5=0 (population=int[0,3], [0,1] for false/true)
+// returns [{city, state}]
+app.get('/quizCities', routes.quizCities)
+
+
+// ********************************************
+//             Trip Routes
+// ********************************************
+
+// DEPRECATED. Leaving it here temporarily until we've fully migrated to the new routes.
+app.get('/trip_pois/', routes.trip_pois)
+
+// Create new trip
+// requires city, state, and personalities --> /new_trip?city=<city>&state=<state>&p0=0&p1=1&...&p5=0 (no quotations, [0,1] for false/true)
+// returns {tripID: int}
+app.get('/new_trip', routes.new_trip)
+
+// Get trip attractions (for tabsCard)
+// requires tripID --> /trip/attractions?tripID=2
+// returns [{pid, subcategory, description, tags, photo}]
+app.get('/trip/attractions', routes.trip_attractions)
+
+// Get trip restaurants (for tabsCard)
+// requires tripID --> /trip/restaurants?tripID=2
+// returns [{pid, subcategory, costLow, costHigh, tags}]
+app.get('/trip/restaurants', routes.trip_restaurants)
+
+// Get trip trails (for tabsCard)
+// requires tripID --> /trip/trails?tripID=2
+// returns [{pid, length, difficulty, low, high, description, route_type, photo}]
+app.get('/trip/trails', routes.trip_trails)
+
+// Delete trip permanently from user account
+// requires tripID --> /delete_trip?tripID=2
+// always returns true
+app.get('/delete_trip', routes.delete_trip)
+
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
