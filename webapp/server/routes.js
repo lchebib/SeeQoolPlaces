cbb7c311ad62ac974e2ab60220427d9cc46413ff
jlim-git <joel.lim.92@gmail.com>
2021-12-01 21:34:37 +0900

    Added logic for recalling and storing trip Favorites and Events

--- webapp/server/routes.js
+++ webapp/server/routes.js
@@ -17,7 +17,7 @@ connection.connect()
 // ********************************************
 
 // Route 1 (handler)
-async function hello(req, res) {
+async function hello (req, res) {
   if (req.query.name) {
     res.send(`Hello, ${req.query.name}! Welcome to the SeeQoolPlaces server!`)
   } else {
@@ -30,7 +30,7 @@ async function hello(req, res) {
 // ********************************************
 
 // Route 1 (handler) - Add new username and password
-async function add_user(req, res) {
+async function add_user (req, res) {
   var username = req.query.username
   var password = req.query.password
 
@@ -71,7 +71,7 @@ async function add_user(req, res) {
 }
 
 // Route 2 (handler) - Log in user
-async function login(req, res) {
+async function login (req, res) {
   var username = req.query.username
   var password = req.query.password
   var clientIP = req.socket.remoteAddress
@@ -141,7 +141,7 @@ async function login(req, res) {
 }
 
 // Route 3 (handler) - Log out user
-async function logout(req, res) {
+async function logout (req, res) {
   var username = req.query.username
   var clientIP = req.socket.remoteAddress
 
@@ -186,7 +186,7 @@ async function logout(req, res) {
 // ********************************************
 
 // Route 2 (handler) - return a random city, and a photo of an attraction or hike
-function random_city(req, res) {
+function random_city (req, res) {
   // top 24 'random' cities that we we select from database to show on landing page
   // BC ideas from here: https://www.planetware.com/canada/best-cities-in-british-columbia-cdn-1-284.htm
   // CA ideas from here: https://www.planetware.com/california/best-places-to-visit-in-california-us-ca-138.htm
@@ -228,7 +228,7 @@ function random_city(req, res) {
 }
 
 // Route 3 (handler) - Return names of existing user-made trips to be displayed in sidebar
-function all_trips(req, res) {
+function all_trips (req, res) {
   var username = req.query.username
 
   var myQuery = `
@@ -254,7 +254,7 @@ function all_trips(req, res) {
 // ********************************************
 
 // Route 5 (handler) - Returns the list of all cities
-function all_cities(req, res) {
+function all_cities (req, res) {
   var myQuery = `
     SELECT DISTINCT state, city
     FROM POI
@@ -275,7 +275,7 @@ function all_cities(req, res) {
 }
 
 // Route 4 (handler) - Returns top 3 cities based on desired city size and travel personalities
-function quizCities(req, res) {
+function quizCities (req, res) {
   var population = req.query.population
   var CoolCat = parseInt(req.query.p0) === 1
   var Adventurer = parseInt(req.query.p1) === 1
@@ -327,7 +327,7 @@ function quizCities(req, res) {
 // ********************************************
 
 // Route 4 (handler) - Filters POIs based on city, state, and personalities. Returns unique tripID.
-function new_trip(req, res) {
+function new_trip (req, res) {
   // get trip profile parameters
   var username = req.query.username
   var tripName = req.query.tripName
@@ -428,7 +428,7 @@ function new_trip(req, res) {
 }
 
 // Route 4 (handler) - Retrieve filtered POIs based on tripID.
-function retrieve_trip(req, res) {
+function retrieve_trip (req, res) {
   var tripID = req.query.tripID
 
   // retrieve trip profile
@@ -477,7 +477,9 @@ function retrieve_trip(req, res) {
             console.log(error)
             res.json({ error: error })
           } else if (results) {
-            console.log('Generated filtered POIs based on city, state, and personalities')
+            console.log(
+              'Generated filtered POIs based on city, state, and personalities'
+            )
             res.json({ results: true })
           }
         })
@@ -560,7 +562,7 @@ function retrieve_trip(req, res) {
 // }
 
 // Route 7 (handler) - Returns the attractions from filtered POIs
-function trip_attractions(req, res) {
+function trip_attractions (req, res) {
   const tripID = req.query.tripID ? req.query.tripID : 0
   var viewName = 'Trip_' + tripID.toString()
 
@@ -582,7 +584,7 @@ function trip_attractions(req, res) {
 }
 
 // Route 8 (handler) - Returns the restaurants from filtered POIs
-function trip_restaurants(req, res) {
+function trip_restaurants (req, res) {
   const tripID = req.query.tripID ? req.query.tripID : 0
   var viewName = 'Trip_' + tripID.toString()
 
@@ -604,7 +606,7 @@ function trip_restaurants(req, res) {
 }
 
 // Route 9 (handler) - Returns the trails from filtered POIs
-function trip_trails(req, res) {
+function trip_trails (req, res) {
   const tripID = req.query.tripID ? req.query.tripID : 0
   var viewName = 'Trip_' + tripID.toString()
 
@@ -625,8 +627,120 @@ function trip_trails(req, res) {
   })
 }
 
+// Route 9 (handler) - Returns the trip Favorited POIs
+function trip_favorites (req, res) {
+  const tripID = req.query.tripID ? req.query.tripID : 0
+
+  var myQuery = `
+    SELECT *
+    FROM TripFavorites NATURAL JOIN POI
+    WHERE tripID = ${tripID};  
+  `
+  console.log(myQuery)
+
+  connection.query(myQuery, function (error, results, fields) {
+    if (error) {
+      console.log(error)
+      res.json({ error: error })
+    } else if (results) {
+      console.log('Got trip Favorites')
+      res.json({ results: results })
+    }
+  })
+}
+
+// Route 9 (handler) - Returns the trip Events
+function trip_events (req, res) {
+  const tripID = req.query.tripID ? req.query.tripID : 0
+
+  var myQuery = `
+    SELECT *
+    FROM TripEvents
+    WHERE tripID = ${tripID};  
+  `
+  console.log(myQuery)
+
+  connection.query(myQuery, function (error, results, fields) {
+    if (error) {
+      console.log(error)
+      res.json({ error: error })
+    } else if (results) {
+      console.log('Got trip Events')
+      res.json({ results: results })
+    }
+  })
+}
+
+// Route 9 (handler) - Save trip Favorites
+function save_favorites (req, res) {
+  const tripID = req.query.tripID ? req.query.tripID : 0
+  const favorites = JSON.parse(req.query.favorites)
+    ? req.query.favorites
+    : { favorites: [] }
+
+  if (favorites.length > 0 && tripID !== 0) {
+    for (let pid of favorites) {
+      var myQuery = `
+        INSERT INTO TripFavorites (tripID, pid)
+        VALUES (${tripID}, ${pid});  
+      `
+      console.log(myQuery)
+
+      connection.query(myQuery, function (error, results, fields) {
+        if (error) {
+          console.log(error)
+          res.json({ error: error })
+        } else if (results) {
+          console.log('Saved trip Favorites')
+          res.json({ results: true })
+        }
+      })
+    }
+  } else {
+    console.log('No favorites to store or invalid trip ID')
+    res.json({ results: false })
+  }
+}
+
+// Route 9 (handler) - Save trip Events
+function save_events (req, res) {
+  const tripID = req.query.tripID ? req.query.tripID : 0
+  const events = JSON.parse(req.query.events)
+    ? req.query.events
+    : { events: [] }
+
+  if (events.length > 0) {
+    for (let event of events) {
+      var eventID = event.eventID
+      var tripID = event.tripID
+      var pid = event.pid
+      var start = event.start
+      var end = event.end
+
+      var myQuery = `
+        INSERT INTO TripEvents (eventID, tripID, pid, start, end)
+        VALUES (${eventID}, ${tripID}, ${pid}, ${start}, ${end});  
+      `
+      console.log(myQuery)
+
+      connection.query(myQuery, function (error, results, fields) {
+        if (error) {
+          console.log(error)
+          res.json({ error: error })
+        } else if (results) {
+          console.log('Saved trip Events')
+          res.json({ results: true })
+        }
+      })
+    }
+  } else {
+    console.log('No events to store')
+    res.json({ results: false })
+  }
+}
+
 // Route 9 (handler) - Deletes trip given tripID
-function delete_trip(req, res) {
+function delete_trip (req, res) {
   const tripID = req.query.tripID ? req.query.tripID : 0
 
   var myQuery = `
@@ -650,7 +764,7 @@ function delete_trip(req, res) {
 //             Filter POIs
 // ********************************************
 
-async function createPersonalityViews() {
+async function createPersonalityViews () {
   // Case 1: Cool Cat
   var CoolCat = `
     CREATE OR REPLACE VIEW CoolCat AS
@@ -879,7 +993,7 @@ async function createPersonalityViews() {
   })
 }
 
-function createPersonalitiesQuery(p0, p1, p2, p3, p4, p5) {
+function createPersonalitiesQuery (p0, p1, p2, p3, p4, p5) {
   var personalities = new Array()
   personalities[0] = new Array(p0, 'CoolCat')
   personalities[1] = new Array(p1, 'Adventurer')
@@ -906,7 +1020,7 @@ function createPersonalitiesQuery(p0, p1, p2, p3, p4, p5) {
   return personalitiesQuery
 }
 
-function createPopulationQuery(pop) {
+function createPopulationQuery (pop) {
   var population = parseInt(pop)
 
   // city size based on https://data.oecd.org/popregion/urban-population-by-city-size.htm
@@ -935,7 +1049,7 @@ function createPopulationQuery(pop) {
   return populationQuery
 }
 
-function generateFilteredPOIs(tripID) {
+function generateFilteredPOIs (tripID) {
   // generate filtered POIs
   createPersonalityViews()
 
@@ -975,7 +1089,7 @@ function generateFilteredPOIs(tripID) {
 //             Authenticate
 // ********************************************
 
-function test(req, res) {
+function test (req, res) {
   var username = req.query.username
   var clientIP = req.socket.remoteAddress
   // console.log('Username: ' + username.toString())
@@ -990,7 +1104,7 @@ function test(req, res) {
   }
 }
 
-function authenticate(username, clientIP) {
+function authenticate (username, clientIP) {
   var myQuery = `
     SELECT *
     FROM LoggedIn
@@ -1028,6 +1142,10 @@ module.exports = {
   trip_attractions,
   trip_restaurants,
   trip_trails,
+  trip_favorites,
+  trip_events,
+  save_favorites,
+  save_events,
   delete_trip,
   test
 }
